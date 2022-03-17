9c465c36caf5b3349d4ae3a65f23a912cf314c02
TheRedMine TheredMaths <thereminedu51@gmail.com>
2022-03-17 16:44:55 +0100

    Update eventsTypes.js

--- v1.5/js/src/video/src/constants/eventsTypes.js
+++ v1.5/js/src/video/src/constants/eventsTypes.js
@@ -19,22 +19,22 @@ export var VOLUME_DOWN = "volumeDown";
 export var TOGGLE_FULLSCREEN_MODE = "fullscreenMode";
 
 /** 48/96 - chapter 0 */
-export var CHAPTER_0 = "0";
+export var CHAPTER_0 = ['chap', 0];
 /** 49/97 - chapter 1 */
-export var CHAPTER_1 = "1";
+export var CHAPTER_1 = ['chap', 1];
 /** 50/98 - chapter 2 */
-export var CHAPTER_2 = "2";
+export var CHAPTER_2 = ['chap', 2];
 /** 51/99 - chapter 3 */
-export var CHAPTER_3 = "3";
+export var CHAPTER_3 = ['chap', 3];
 /** 52/100 - chapter 4 */
-export var CHAPTER_4 = "4";
+export var CHAPTER_4 = ['chap', 4];
 /** 53/101 - chapter 5 */
-export var CHAPTER_5 = "5";
+export var CHAPTER_5 = ['chap', 5];
 /** 54/102 - chapter 6 */
-export var CHAPTER_6 = "6";
+export var CHAPTER_6 = ['chap', 6];
 /** 55/103 - chapter 7 */
-export var CHAPTER_7 = "7";
+export var CHAPTER_7 = ['chap', 7];
 /** 56/104 - chapter 8 */
-export var CHAPTER_8 = "8";
+export var CHAPTER_8 = ['chap', 8];
 /** 57/105 - chapter 9 */
-export var CHAPTER_9 = "9";
+export var CHAPTER_9 = ['chap', 9];
