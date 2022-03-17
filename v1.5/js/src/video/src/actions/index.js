2aeba9e71006df3f185201609cd2a7ed706984ff
TheRedMine TheredMaths <thereminedu51@gmail.com>
2022-03-17 16:44:48 +0100

    Update index.js

--- v1.5/js/src/video/src/actions/index.js
+++ v1.5/js/src/video/src/actions/index.js
@@ -1,13 +1,16 @@
 import {
-    PLAY_OR_PAUSE, 
-    TOGGLE_FULLSCREEN_MODE, 
-    VOLUME_DOWN, 
-    VOLUME_UP
+    CHAPTER_0, CHAPTER_1,
+    CHAPTER_2, CHAPTER_3,
+    CHAPTER_4, CHAPTER_5,
+    CHAPTER_6, CHAPTER_7,
+    CHAPTER_8, CHAPTER_9,
+    PLAY_OR_PAUSE, TOGGLE_FULLSCREEN_MODE, 
+    VOLUME_DOWN, VOLUME_UP
 } from '../constants/eventsTypes.js';
 
 
 /**
- * @param {String} action
+ * @param {String|Array} action
  * @returns {void|null|string|boolean}
  */
 
@@ -15,16 +18,14 @@ export default (action) => {
     if (undefined === __video || null === video)
         var __video = window.__video;
     
-    switch (action) {
+    switch (
+        typeof action == 'string' ? action : action[0]
+    ) {
         case PLAY_OR_PAUSE:
             return __video[__video.paused ? 'play' : 'pause']();
 
         case TOGGLE_FULLSCREEN_MODE:
-            var isFullscreen = new Boolean(true === (document.fullscreenElement && document.fullscreenElement.nodeName == 'VIDEO'));
-
-            __video.dataset.fullscreen = isFullscreen.toString();
-
-            if (__video.dataset.fullscreen == 'true') {
+            if (__video.dataset.fullscreen == true) {
                 try {
                     document.exitFullscreen();
                 } catch {
@@ -33,7 +34,7 @@ export default (action) => {
             } else {
                 var requestMethod = __video.requestFullScreen || __video.webkitRequestFullScreen || __video.mozRequestFullScreen || __video.msRequestFullScreen;
             }
-                                
+            
             if (requestMethod) {
                 requestMethod.call(__video);
             } else if (typeof window.ActiveXObject !== 'undefined') {
@@ -42,6 +43,8 @@ export default (action) => {
                 if (null !== wscript) wscript.SendKeys("{F11}");
             }
 
+            __video.dataset.fullscreen = true == __video.dataset.fullscreen ? 0 : 1;
+
             break;
 
         case VOLUME_UP:
@@ -49,5 +52,14 @@ export default (action) => {
 
         case VOLUME_DOWN:
             return __video.volume -= 10;
+
+        case CHAPTER_0[0]: case CHAPTER_1[0]:
+        case CHAPTER_2[0]: case CHAPTER_3[0]:
+        case CHAPTER_4[0]: case CHAPTER_5[0]:
+        case CHAPTER_6[0]: case CHAPTER_7[0]:
+        case CHAPTER_8[0]: case CHAPTER_9[0]:
+            return __video.currentTime = __video.duration / 10 * action[1];
+        
+        default: return;
     }
 };
