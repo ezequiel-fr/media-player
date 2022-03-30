6df5208d37bc5a297ad4a83aa7ccebe88ee55878
TheRedMine TheredMaths <theredminedu51@gmail.com>
2022-03-30 13:19:34 +0200

    Change null value

--- v1.5/js/src/video/src/build/cookies.js
+++ v1.5/js/src/video/src/build/cookies.js
@@ -18,7 +18,7 @@ export function setCookie (name, value) {
  * Get cookie value
  * 
  * @param {String} name the name of the cookie
- * @returns {String} response
+ * @returns {null} response
  */
 
 export function getCookie (name) {
@@ -35,7 +35,7 @@ export function getCookie (name) {
         }
     }
 
-    return 'null';
+    return null;
 }
 
 
